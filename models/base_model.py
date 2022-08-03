15a1005fbb2de223af36a64b709829c1fc07c80d
devmutinda <davidkymutinda@gmail.com>
2022-08-03 18:28:58 +0000

    Added storage object to models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This module contains Basemodel class
 """
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """
@@ -26,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns string rep of instance"""
@@ -34,6 +35,8 @@ class BaseModel:
     def save(self):
         """Updates attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
