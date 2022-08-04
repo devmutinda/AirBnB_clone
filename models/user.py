3aef343c4df94cf841b5a5631d7362b97ee11cf6
devmutinda <davidkymutinda@gmail.com>
2022-08-04 11:43:30 +0000

    Created models/user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+User inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
