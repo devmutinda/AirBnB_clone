51c8f13dfaf85b481347ea383b72c40ad9b8c908
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-08-05 00:55:16 +0300

    Console

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ This module activates python cli
 """
 import cmd
 import sys
+from models.base_model import BaseModel
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,8 +19,27 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_quit(self, arg):
-        """Exits the python shell:  QUIT"""
+        """Quit command to exit the program"""
         quit()
+
+    def do_create(self, args):
+        """Creates a new instance of a class"""
+        classes = ['BaseModel']
+        arg2 = shlex.split(args)
+        if len(arg2) < 1:
+            print("**class name missing **")
+            return
+        if arg2[0] not in classes:
+            print("**class doesn't exist **")
+            return
+        model = BaseModel()
+        model.save()
+        print(model.id)
+
+    def emptyline(self):
+        pass
+
+        return
 
 
 if __name__ == '__main__':
--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -11,27 +11,35 @@ import io
 import os
 import sys
 
+
 class storageTest(unittest.TestCase):
-        """Testing storage access"""
-        def test_save(self):
-                """test saving of new obj"""
-                model = BaseModel()
-                x = model.id
-                y = "BaseModel.{}".format(x)
-                model.save()
-                with open("file.json", 'r', encoding="utf-8") as f:
-                        data = json.loads(f.read())
-                self.assertEqual(y in data.keys(), True)
-        def test_newobj(self):
-                """test self.__ojects in filestorage"""
-                storage = FileStorage()
-                model = BaseModel()
-                x = model.id
-                y = "BaseModel.{}".format(x)
-                storage.new(model)
-                z = storage.all()
-                self.assertEqual(y in z.keys(), True)
-                
+    """Testing storage access"""
 
+    def test_save(self):
+        """test saving of new obj"""
+        model = BaseModel()
+        x = model.id
+        y = "BaseModel.{}".format(x)
+        model.save()
+        with open("file.json", 'r', encoding="utf-8") as f:
+            data = json.loads(f.read())
+        self.assertEqual(y in data.keys(), True)
 
+    def test_newobj(self):
+        """test self.__ojects in filestorage"""
+        storage = FileStorage()
+        model = BaseModel()
+        x = model.id
+        y = "BaseModel.{}".format(x)
+        storage.new(model)
+        z = storage.all()
+        self.assertEqual(y in z.keys(), True)
 
+    def test_reload(self):
+        """if file does not exist"""
+        storage = FileStorage()
+        os.rename('file.json', 'file2.json')
+        storage.reload()
+        z = storage.all()
+        self.assertEqual(z, {})
+        os.rename('file2.json', 'file.json')
